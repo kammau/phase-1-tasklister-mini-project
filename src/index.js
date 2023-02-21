0437eefde707e6c325871e16a7a97c4c565496ae
Kamryn Smith <programersign@gmail.com>
2023-02-21 10:41:22 -0800

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  function listBuilder(value) {
+    let li = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", deleteHandler);
+    btn.textContent = "Delete"
+    li.textContent = `${value} `
+    li.appendChild(btn)
+    document.getElementById("tasks").appendChild(li)
+  }
+
+  function deleteHandler(value) {
+    value.target.parentNode.remove()
+  }
+  
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    listBuilder(event.target["new-task-description"].value)
+    form.reset();
+  })
 });
+
